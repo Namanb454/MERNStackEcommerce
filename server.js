563222679b9ee5a62c4e4369e6840c27ad2a7de0
Naman <namanb454@gmail.com>
2024-01-30 18:48:14 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js'
+import authRoute from './routes/authRoute.js'
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import cors from 'cors'
+
+
+// configure env 
+dotenv.config()
+
+//database config
+connectDB();
+
+// rest app 
+const app = express()
+
+//Middelwares
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/products', productRoutes);
+
+// rest api
+app.get('/', (req, res) => {
+    res.send("<h1>welcome to ecommerce app</h1 >");
+})
+
+//Port
+const PORT = process.env.PORT || 8080;
+
+//run Listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`.bgGreen);
+});
